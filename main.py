ca95d65023d1148c8ede2b0a1c694cfedaef6156
wanna_get_out <inv.moiseev@gmail.com>
2022-06-26 18:29:07 +0300

    Initial project version

---
+++ main.py
@@ -0,0 +1,10 @@
+import parser
+import time
+import random
+
+if __name__ == "__main__":
+    for page in range(378, 1230):   #перебор номеров страниц по образцу page='num'
+        u = 'https://kino.mail.ru/cinema/all/?order=name&page=' + str(page)
+        parser.parse(u) #вызов парсера
+        print(u) #чек-поинт считанной страницы
+        time.sleep(float(random.randint(5, 15)))    #задержка между запросами для избежания капчи
