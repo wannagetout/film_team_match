0619201ba526d6afa094a6f306318d6a1ee4683a
wannagetout <inv.moiseev@gmail.com>
2022-07-04 21:40:12 +0300

    get text from soup

--- db/parser.py
+++ db/parser.py
@@ -14,29 +14,25 @@ def parse(url):
     film_info = soup.find_all('div', class_='margin_top_5')
     film_rate = soup.find_all('span', class_='p-rate-flag__text')
 
+    for name in film_names:
+         name = name.get_text()
+         print(name)
+    
     #print(type(film_names))
     #print(type(film_info))
     #print(type(film_rate))
-    
-    for name in film_names:
-        print(name.text)
-    
-    
-    for info in film_info:
-    
-        print(info.text)
+    #print(film_names)
+    #print(film_info)
+    #print(film_rate)
+    #film_state = (film_names + film_info + film_rate).get_text()
+    #print(film_state)
+
         
-    
-    for rate in film_rate:
-        
-        print(rate.text)
-    film_state = (film_names + film_info + film_rate)
-    
-    print(film_state.text)
     #with open('film_list_by_pandas.csv', 'w', newline='') as films: #csv файл с добавлением данных из цикла
     #    field_names = ['film_name',
     #                   'film_imfo',
     #                   'film_rate']
+    #    data = pd.DataFrame(film_state, columns=field_names)
     #    
     #    for info in film_info:
     #        data = pd.DataFrame(films, columns=field_names)
