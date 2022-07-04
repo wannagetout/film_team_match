c9578aad3879bdee01ee5524d2c7f9166a00522f
wannagetout <inv.moiseev@gmail.com>
2022-07-04 22:00:35 +0300

    get text from soupx2

--- db/parser.py
+++ db/parser.py
@@ -13,11 +13,20 @@ def parse(url):
     film_names = soup.find_all('span', class_='link__text')
     film_info = soup.find_all('div', class_='margin_top_5')
     film_rate = soup.find_all('span', class_='p-rate-flag__text')
-
+    film_names_list = []
+    film_info_list = []
+    film_rate_list = []
     for name in film_names:
          name = name.get_text()
-         print(name)
+         film_names_list.append(name)
+         
+    for info in film_info:
+         info = info.get_text()
+         film_info_list.append(info)
     
+    for rate in film_rate:
+         rate = rate.get_text()
+         film_rate_list.append(rate)
     #print(type(film_names))
     #print(type(film_info))
     #print(type(film_rate))
@@ -28,11 +37,13 @@ def parse(url):
     #print(film_state)
 
         
-    #with open('film_list_by_pandas.csv', 'w', newline='') as films: #csv файл с добавлением данных из цикла
-    #    field_names = ['film_name',
-    #                   'film_imfo',
-    #                   'film_rate']
-    #    data = pd.DataFrame(film_state, columns=field_names)
+    with open('film_list_by_pandas.csv', 'w', newline='') as films: #csv файл с добавлением данных из цикла
+        field_names = ['film_name',
+                       'film_imfo',
+                       'film_rate']
+        film_name_data = pd.DataFrame(film_names_list, columns=field_names[0])
+        film_info_data = pd.DataFrame(film_info_list, columns=field_names[1])
+        film_rate_data = pd.DataFrame(film_rate_list, columns=field_names[2])
     #    
     #    for info in film_info:
     #        data = pd.DataFrame(films, columns=field_names)
