946a543578effd5e831a1345ebbb05201bc5909b
wannagetout <inv.moiseev@gmail.com>
2022-07-09 08:38:38 +0300

    making true urls on link column

--- db/parser.py
+++ db/parser.py
@@ -1,3 +1,4 @@
+from msilib.schema import Error
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
@@ -9,13 +10,17 @@ def parse(url):
      }
      response = requests.get(url, headers=HEADERS)
      soup = BeautifulSoup(response.content, 'html.parser')
-
+     film_rate = ''
      film_names = soup.find_all('a', class_='link link_inline link-holder link-holder_itemevent link-holder_itemevent_small')
      film_info = soup.find_all('div', class_='margin_top_5')
-     film_rate = soup.find_all('span', class_='p-rate-flag__text') and soup.find_all('span', class_='p-rate-flag__imdb-text')
+     film_link = soup.find_all('a', class_='link link_inline link-holder link-holder_itemevent link-holder_itemevent_small')
+     #film_rate_1 = (soup.find_all('span', class_='p-rate-flag__text')) 
+     #film_rate_2 = (soup.find_all('span', class_='p-rate-flag__imdb-text'))
+     
      film_names_list = []
      film_info_list = []
-     film_rate_list = []
+     #film_rate_list = []
+     film_link_list = []
      for name in film_names:
           name = name.get_text()
           film_names_list.append(name)
@@ -26,11 +31,17 @@ def parse(url):
           film_info_list.append(info)
           #print(info)
           
+     for link in film_link:
+          link = link.get('href')
+          film_link_list.append('https://kino.mail.ru/' + link)
           
-     for rate in film_rate:
-          rate = rate.get_text()
-          film_rate_list.append(rate)
-          #print(rate)
+     #for rate in film_rate_1 and film_rate_2:
+     #     if rate == str(rate):
+     #          film_rate_list.append(rate)
+     #     else:
+     #          rate = rate.get_text()
+     #          film_rate_list.append(rate)
+     #    print(rate)
      #print(type(film_names))
      #print(type(film_info))
      #print(type(film_rate))
@@ -47,6 +58,7 @@ def parse(url):
           'names': film_names_list,
           'info': film_info_list,
           #'rate': film_rate_list
+          'link': film_link_list
           }
 #----------------------------------------------------------------------------------------------------------------
      #field_names = ['film_name',
@@ -66,7 +78,7 @@ def parse(url):
      #film_info_data.to_csv('film_list_by_pandas', columns=fiesld_info)
      #film_rate_data.to_csv('film_list_by_pandas', columns=film_rate)
      
-     dframe_names.to_csv('film_list_by_pandas.csv', encoding='utf-8', mode='a')
+     dframe_names.to_csv('film_list.csv', encoding='utf-8', mode='a')
      #dframe_info.to_csv('film_list_by_pandas.csv', encoding='utf-8', mode='a')
      #dframe_rate.to_csv('film_list_by_pandas.csv', encoding='utf-8', mode='a')
      
