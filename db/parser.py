a8c3933c91f122c9f10cbcde31b78d56f760d961
wannagetout <inv.moiseev@gmail.com>
2022-07-13 05:06:54 +0300

    _

--- db/parser.py
+++ db/parser.py
@@ -10,83 +10,33 @@ def parse(url):
      }
      response = requests.get(url, headers=HEADERS)
      soup = BeautifulSoup(response.content, 'html.parser')
-     film_rate = ''
      film_names = soup.find_all('a', class_='link link_inline link-holder link-holder_itemevent link-holder_itemevent_small')
      film_info = soup.find_all('div', class_='margin_top_5')
      film_link = soup.find_all('a', class_='link link_inline link-holder link-holder_itemevent link-holder_itemevent_small')
-     #film_rate_1 = (soup.find_all('span', class_='p-rate-flag__text')) 
-     #film_rate_2 = (soup.find_all('span', class_='p-rate-flag__imdb-text'))
-     
+  
      film_names_list = []
      film_info_list = []
-     #film_rate_list = []
      film_link_list = []
      for name in film_names:
           name = name.get_text()
           film_names_list.append(name)
-          #print(name)
           
      for info in film_info:
           info = info.get_text()
           film_info_list.append(info)
-          #print(info)
-          
+         
      for link in film_link:
           link = link.get('href')
           film_link_list.append('https://kino.mail.ru/' + link)
-          
-     #for rate in film_rate_1 and film_rate_2:
-     #     if rate == str(rate):
-     #          film_rate_list.append(rate)
-     #     else:
-     #          rate = rate.get_text()
-     #          film_rate_list.append(rate)
-     #    print(rate)
-     #print(type(film_names))
-     #print(type(film_info))
-     #print(type(film_rate))
-     #print(film_names)
-     #print(film_info)
-     #print(film_rate)
-     #film_state = (film_names + film_info + film_rate).get_text()
-     #print(film_state)
-     #names_series = pd.Series(film_names_list)
-     #info_series = pd.Series(film_info_list)
-     #rate_series = pd.Series(film_rate_list)
-     
+              
      film_data = {
           'names': film_names_list,
           'info': film_info_list,
-          #'rate': film_rate_list
+
           'link': film_link_list
           }
-#----------------------------------------------------------------------------------------------------------------
-     #field_names = ['film_name',
-     #          'film_imfo',
-     #          'film_rate']
-     
-     #film_names = pd.DataFrame(names_series, columns=field_names)
-     #film_info_data = pd.DataFrame(info_series, columns=field_info)
-     #film_rate_data = pd.DataFrame(rate_series, columns=film_rate)
      print(film_data)
-     #data = [names_series + info_series + rate_series]
-     
+    
      dframe_names = pd.DataFrame(data=film_data)
-     #dframe_info = pd.DataFrame(data=film_data['info'])
-     #dframe_rate = pd.DataFrame(data=film_data['rate'])
-     
-     #film_info_data.to_csv('film_list_by_pandas', columns=fiesld_info)
-     #film_rate_data.to_csv('film_list_by_pandas', columns=film_rate)
-     
+
      dframe_names.to_csv('film_list.csv', encoding='utf-8', mode='a')
-     #dframe_info.to_csv('film_list_by_pandas.csv', encoding='utf-8', mode='a')
-     #dframe_rate.to_csv('film_list_by_pandas.csv', encoding='utf-8', mode='a')
-     
-     #print(dframe)
-     #print(film_info_data)
-     #print(film_rate_data)
-     #    
-     #    for info in film_info:
-     #        data = pd.DataFrame(films, columns=field_names)
-
-
--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-from db import parser 
+from db import parser
+from db import take_link_from_csv
 import time
 import random
 
 if __name__ == "__main__":
-    for page in range(1, 1231):   #перебор номеров страниц по образцу page='num'
-        u = 'https://kino.mail.ru/cinema/all/?order=name&page=' + str(page)
-        parser.parse(u) #вызов парсера
-        print(u) #чек-поинт считанной страницы
-        time.sleep(float(random.randint(5, 15)))    #задержка между запросами для избежания капчи
+    while True:
+        print('1 - parse, 2 - read_csv, 3 - exit')
+        user_input = input()
+        if user_input == '1':    
+            for page in range(1, 1231):   #перебор номеров страниц по образцу page='num'
+                u = 'https://kino.mail.ru/cinema/all/?order=name&page=' + str(page)
+                parser.parse(u) #вызов парсера
+                print('-----------------------------------------------------')
+                print(u, 'done') #чек-поинт считанной страницы
+                print('-----------------------------------------------------')
+                time.sleep(float(random.randint(5, 15)))    #задержка между запросами для избежания капчи
+        if user_input == '2':
+            take_link_from_csv.read_from_csv()
+            #take_link_from_csv.take_content_from_link()
+        if user_input == '3':
+            exit()
